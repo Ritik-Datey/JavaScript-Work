f79a106ca9329ad564b0eac76b9670bbfdcaa1f5
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-06-03 06:11:11 +0000

    Basic of arrays learn

---
+++ Programming_Practice/Arrays.js
@@ -0,0 +1,6 @@
+// We can create an array in two ways directly ot by using new keywords...
+ let array1=[1,2,3,4,5];
+ //console.log(array1);
+
+ let array2=new Array(1,2,3,4,5);
+ //console.log(array2);
