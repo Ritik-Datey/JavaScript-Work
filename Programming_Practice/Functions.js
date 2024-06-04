dbe29aeff345f747b31f19060a5b336389e6c058
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-06-04 16:46:34 +0000

    ...

---
+++ Programming_Practice/Functions.js
@@ -0,0 +1,14 @@
+//Printing function...
+function print()
+{
+    console.log("Hello Brother welcome to Java");
+}
+//print(); // Hello Brother welcome to Java
+
+// return function
+function print()
+{
+    return "Hello Brother welcome to Java";
+}
+const p=print();
+//console.log(p);
