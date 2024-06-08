620a507020a228232bca1655542ef6e9719be448
Ritik vijay Datey <ritikdatey2017@gmail.com>
2024-06-08 06:33:56 +0000

    Events basic

---
+++ Programming_Practice/Events/main.js
@@ -0,0 +1,10 @@
+//Normal apporch
+document.getElementById('blackGoku').onclick = function() {
+    alert("Goku Wallpaper with black background and this is done by normal apporach");
+}
+
+// Best way .. it give us more fuctionality
+document.getElementById("audiRs7").addEventListener("click", function()
+{
+    alert("Audi RS7 mansory Addition and this is done by addEventListener apporach");
+})
